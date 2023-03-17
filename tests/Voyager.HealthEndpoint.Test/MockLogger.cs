26795e3e14867d5a2056ab94ebbfe1b7fde334bb
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2023-03-17 22:42:57 +0100

    Wypchnięcie pakietu

--- tests/Voyager.HealthEndpoint.Test/MockLogger.cs
+++ tests/Voyager.HealthEndpoint.Test/MockLogger.cs
@@ -1,67 +1,65 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace Voyager.HealthEndpoint.Test
+﻿namespace Voyager.HealthEndpoint.Test
 {
-  class MockLogger<TCategoryName> : TestScopeProcess, ILogger<TCategoryName>
-  {
-    public IDisposable BeginScope<TState>(TState state)
-    {
-      return new LogScope(this);
-    }
+	class MockLogger<TCategoryName> : ScopeProcess, ILogger<TCategoryName>
+	{
+		public IDisposable BeginScope<TState>(TState state)
+		{
+			return new LogScope(this);
+		}
 
-    public bool IsEnabled(LogLevel logLevel)
-    {
-      return true;
-    }
+		public bool IsEnabled(LogLevel logLevel)
+		{
+			return true;
+		}
 
-    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
-    {
-      Console.WriteLine(GetSpacess() + formatter.Invoke(state, null));
-    }
+		public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+		{
+			Console.WriteLine(GetSpacess() + formatter.Invoke(state, exception));
+		}
 
-    int spacesCount = 0;
+		int spacesCount = 0;
 
-    string GetSpacess()
-    {
-      string s = "";
-      for (int i = 0; i < spacesCount; i++)
-        s += " ";
-      return s;
+		string GetSpacess()
+		{
+			string s = "";
+			for (int i = 0; i < spacesCount; i++)
+				s += " ";
+			return s;
 
-    }
-
-
-    public void ScopeEnter()
-    {
-      spacesCount += 2; 
-    }
-    public void ScopeExit()
-    {
-      spacesCount -= 2; 
-    }
+		}
 
 
-    class LogScope : IDisposable
-    {
-      private readonly TestScopeProcess owner;
+		public void ScopeEnter()
+		{
+			spacesCount += 2;
+		}
+		public void ScopeExit()
+		{
+			spacesCount -= 2;
+		}
 
-      public LogScope(TestScopeProcess owner)
-      {
-        this.owner = owner;
-        this.owner.ScopeEnter();
-      }
 
-      public void Dispose()
-      {
-        this.owner.ScopeExit();
-      }
-    }
-  }
+		class LogScope : IDisposable
+		{
+			private readonly ScopeProcess owner;
 
-  internal interface TestScopeProcess
-  {
-    void ScopeEnter();
-    public void ScopeExit();
-  }
+			public LogScope(ScopeProcess owner)
+			{
+				this.owner = owner;
+				this.owner.ScopeEnter();
+			}
+
+			public void Dispose()
+			{
+				this.owner.ScopeExit();
+			}
+		}
+	}
+
+	internal interface ScopeProcess
+	{
+		void ScopeEnter();
+		public void ScopeExit();
+	}
 
 }
